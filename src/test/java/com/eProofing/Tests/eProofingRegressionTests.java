622b31193069250662b005ab5d5e9f442d070075
Vidya Suryavanshi <vidya.suryavanshi@springernature.com>
2018-08-10 16:49:23 +0530

    test commit

--- src/test/java/com/eProofing/Tests/eProofingRegressionTests.java
+++ src/test/java/com/eProofing/Tests/eProofingRegressionTests.java
@@ -8,74 +8,76 @@ import com.eProofing.Utilities.SeleniumWaits;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
-
 import org.testng.annotations.BeforeMethod;
-
-import java.io.File;
 import java.io.IOException;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 
-public class eProofingRegressionTests extends eProofingProperties{
- 
+public class eProofingRegressionTests {
+
 	ExtentReports extent;
 	ExtentTest logger;
-    WebDriver driver;
-    eProofingHomePageObjects ehp;
+	WebDriver driver;
+	eProofingHomePageObjects ehp;
+	eProofingProperties eprop;
 
-  @BeforeMethod
-  public void beforeMethod() 
-  {
-	 driver = new ChromeDriver();
-	 System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-	 driver.get(eProofingURL);
-	 
-	 ehp = new eProofingHomePageObjects(driver);
-	 
-	 extent = new ExtentReports(System.getProperty("user.dir")+"\\EproofingExtentReports\\"+"RegressionReport"+".html");
-	 //extent.addSystemInfo("Host Name", "SoftwareTestingMaterial").addSystemInfo("Environment", "Automation Testing").addSystemInfo("User Name", "Vidya Suryavanshi");
-     //extent.loadConfig(new File(System.getProperty("user.dir")+"\\extent-config.xml"));
-  }
-  
-  
-  @Test
-  public void startProofing () 
-  {
-	  logger = extent.startTest("Eproofing Test");
-	  ehp.startProof();
-	  logger.log(LogStatus.INFO, "Eproofing Started!");
-  }
+	@BeforeMethod
+	public void beforeMethod() {
 
-  @AfterMethod
-  
-	  public void afterMethod(ITestResult results) throws IOException
-	  {
-		if(results.getStatus() == ITestResult.FAILURE)
-		{
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		eprop = new eProofingProperties();
+
+		ehp = new eProofingHomePageObjects(driver);
+
+		driver.get(eprop.eProofingURL);
+
+		extent = new ExtentReports(
+				System.getProperty("user.dir") + "\\EproofingExtentReports\\" + "RegressionReport" + ".html");
+		// extent.addSystemInfo("Host Name",
+		// "SoftwareTestingMaterial").addSystemInfo("Environment", "Automation
+		// Testing").addSystemInfo("User Name", "Vidya Suryavanshi");
+		// extent.loadConfig(new
+		// File(System.getProperty("user.dir")+"\\extent-config.xml"));
+	}
+
+	@Test
+	public void startProofing() {
+		logger = extent.startTest("Eproofing Test");
+		ehp.startProof();
+		logger.log(LogStatus.INFO, "Eproofing Started!");
+	}
+
+	@AfterMethod
+
+	public void afterMethod(ITestResult results) throws IOException {
+		if (results.getStatus() == ITestResult.FAILURE) {
 			String screenShotPath = SeleniumWaits.capture(driver, "Eproofing_FailureScreenShot");
-			
-				logger.log(LogStatus.FAIL, "Test case Failed is: " + results.getName());
-				logger.log(LogStatus.FAIL, "Test case Failed is: " + results.getThrowable());
-				logger.log(LogStatus.FAIL, "Test case Failed is: " + logger.addScreenCapture(screenShotPath));
-				
-			} else if (results.getStatus() == ITestResult.SKIP) {
-				
-				logger.log(LogStatus.SKIP, "Test case Skipped is: " + results.getName());
-				
-			} 		
-	  
-	  extent.endTest(logger);
-	       
-  }
-  
-  @AfterTest
-  public void teardown(){
-	  
-	  extent.flush();
-	  driver.quit(); 
-  }
-  }
+
+			logger.log(LogStatus.FAIL, "Test case Failed is: " + results.getName());
+			logger.log(LogStatus.FAIL, "Test case Failed is: " + results.getThrowable());
+			logger.log(LogStatus.FAIL, "Test case Failed is: " + logger.addScreenCapture(screenShotPath));
+
+		} else if (results.getStatus() == ITestResult.SKIP) {
+
+			logger.log(LogStatus.SKIP, "Test case Skipped is: " + results.getName());
+
+		}
+
+		extent.endTest(logger);
+
+	}
+
+	@AfterTest
+	public void teardown() {
+
+		extent.flush();
+		driver.quit();
+	}
+}
